7daef813102e1ca5789786cb885c5ae3cb1f0212
navanee1609 <navaneethanvs18@gmail.com>
2023-12-13 12:42:36 +0530

    new file added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Ecommerce</h2>
+     <p>piiuy</p>
+    </div>
+  );
+}
+
+export default App;
