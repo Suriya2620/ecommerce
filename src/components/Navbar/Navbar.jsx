731d18e1bfec3650eed63424bb712ed74ad9c88b
navanee1609 <navaneethanvs18@gmail.com>
2023-12-13 22:10:35 +0530

    completed hero section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
    <div className='navbar'>
     <div className="nav-logo">
     <img src={logo} alt="" />
-    <p>Shopper</p>
+    <p>Shopzy</p>
     </div>
 
     <ul className="nav-menu">
