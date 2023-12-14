e51d8073a7febc6674c7912d8e11c3e481ff76e8
navanee1609 <navaneethanvs18@gmail.com>
2023-12-14 08:59:11 +0530

    designed shop category products

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import Product from './pages/Product'
 import Cart from './pages/Cart'
 import LoginSignup from './pages/LoginSignup'
 import Footer from './components/Footer/Footer'
+import men_banner from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kid_banner from './components/Assets/banner_kids.png'
 const App = () => {
   return (
     <div>
@@ -17,9 +20,9 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path='/'element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category='men'/>}/>
-        <Route path='/womens' element={<ShopCategory category='women'/>}/>
-        <Route path='/kids' element={<ShopCategory category='kid'/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category='men'/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category='women'/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category='kid'/>}/>
         <Route path='/product' element={<Product/>}>
 
           <Route path=':productId' element='<Product/>'/>
