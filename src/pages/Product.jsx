624b88076b9967265e1f3899f9dfd534bf098ac1
navanee1609 <navaneethanvs18@gmail.com>
2023-12-14 10:27:27 +0530

    added breadcrumb feature

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {ShopContext} from '../context/ShopContext'
+import { useParams } from 'react-router-dom'
+import BreadCrumb from '../components/BreadCrumb/BreadCrumb'
 const Product = () => {
+  const {all_product} =useContext(ShopContext)
+  let {productId}=useParams()
+  let product = all_product.find((e)=>e.id === Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      <BreadCrumb product={product}/>
+    </div>
   )
 }
 
