e650b262b7d3f4a415d6a8a53d3a744a833ae353
navanee1609 <navaneethanvs18@gmail.com>
2023-12-14 10:13:59 +0530

    added login signup form

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import Shop from './pages/Shop'
 import ShopCategory from './pages/ShopCategory'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
-import LoginSignup from './pages/LoginSignup'
+
 import Footer from './components/Footer/Footer'
 import men_banner from './components/Assets/banner_mens.png'
 import women_banner from './components/Assets/banner_women.png'
 import kid_banner from './components/Assets/banner_kids.png'
+import LoginSignup from './pages/LoginSignup'
 const App = () => {
   return (
     <div>
